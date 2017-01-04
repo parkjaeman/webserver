35850a33f7ac79920a9cc79d96ad41251b00602b
Jaeman Park <jaeman1.park@samsung.com>
2017-01-04 12:31:07 +0000

    Complete simple CICD by hooking from github

--- deploy.sh
+++ deploy.sh
@@ -1,19 +1,20 @@
-echo "aaa"
+# aws configure
 aws configure set aws_access_key_id AKIAJWZQFLWB4QLGM3RQ
 aws configure set aws_secret_access_key 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
 aws configure set region ap-southeast-1
 aws configure set output json
-echo "bbb"
 
-#: <<'END'
+# aws login
 aws ecr get-login --region ap-southeast-1 > temp.sh
 chmod +x temp.sh
 ./temp.sh
 rm temp.sh
 
+# update image in repository
 docker build -t 834160605896.dkr.ecr.ap-southeast-1.amazonaws.com/webserver:latest .
 docker push 834160605896.dkr.ecr.ap-southeast-1.amazonaws.com/webserver:latest
+
+# deployment as the updated image
 curl -X DELETE -H "cluster_name: toledo-dev-v0.4" -H "service_name: webserver" http://52.221.249.75:8080/userService
 sleep 10
 curl --data-binary @webserver.yaml -H "Content-Type: text/plain" -H "cluster_name: toledo-dev-v0.4" -H "service_name: webserver" -H "command: create" -H "flags: -f" http://52.221.249.75:8080/userService
-#END
