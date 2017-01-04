f88bb1eda69d54f2674150ac7026c367a4503a17
Jaeman Park <jaeman1.park@samsung.com>
2017-01-04 12:18:40 +0000

    1

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,11 @@
 echo "aaa"
-aws configure set profile.prod.aws_access_key AKIAJWZQFLWB4QLGM3RQ
-aws configure set profile.prod.aws_secret_access_key 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
-aws configure set profile.prod.region ap-southeast-1
-aws configure set profile.prod.output json
+aws configure set --profile=mf aws_access_key AKIAJWZQFLWB4QLGM3RQ
+aws configure set --profile=mf aws_secret_access_key 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
+aws configure set --profile=mf region ap-southeast-1
+aws configure set --profile=mf output json
 echo "bbb"
 
-: <<'END'
+#: <<'END'
 aws ecr get-login --region ap-southeast-1 > temp.sh
 chmod +x temp.sh
 ./temp.sh
@@ -16,4 +16,4 @@ docker push 834160605896.dkr.ecr.ap-southeast-1.amazonaws.com/webserver:latest
 curl -X DELETE -H "cluster_name: toledo-dev-v0.4" -H "service_name: webserver" http://52.221.249.75:8080/userService
 sleep 10
 curl --data-binary @webserver.yaml -H "Content-Type: text/plain" -H "cluster_name: toledo-dev-v0.4" -H "service_name: webserver" -H "command: create" -H "flags: -f" http://52.221.249.75:8080/userService
-END
+#END
