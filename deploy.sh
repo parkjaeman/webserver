e5aa964fe949458faa883e2b4a3892afde02db9c
Jaeman Park <jaeman1.park@samsung.com>
2017-01-04 09:12:27 +0000

    Implement webserver based on http

---
+++ deploy.sh
@@ -0,0 +1,5 @@
+docker build -t 834160605896.dkr.ecr.ap-southeast-1.amazonaws.com/webserver:latest .
+docker push 834160605896.dkr.ecr.ap-southeast-1.amazonaws.com/webserver:latest
+curl -X DELETE -H "cluster_name: toledo-dev-v0.4" -H "service_name: webserver" http://52.221.249.75:8080/userService
+sleep 20
+curl --data-binary @webserver.yaml -H "Content-Type: text/plain" -H "cluster_name: toledo-dev-v0.4" -H "service_name: webserver" -H "command: create" -H "flags: -f" http://52.221.249.75:8080/userService
---
+++ server.js
@@ -0,0 +1,18 @@
+var str = 'Hello World!';
+var http = require('http');
+var resCount = 0;
+var id = Math.random();
+var handleRequest = function(request, response) {
+  console.log('Received request for URL: ' + request.url);
+  resCount++;
+  var resStr = '';
+  for (var i = 0; i < 1000; i++) {
+    var tmp = Math.random();
+    resStr += str + tmp + resCount;
+  }
+
+  response.writeHead(200);
+  response.end(id +', Hello World! 0, ' + resStr.length);
+};
+var www = http.createServer(handleRequest);
+www.listen(8080);
