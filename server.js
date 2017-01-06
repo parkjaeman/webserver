d0decb1cc44cd480af4452195ba30dadb137f650
Jaeman Park <jaeman1.park@samsung.com>
2017-01-06 01:08:53 +0000

    6

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ var handleRequest = function(request, response) {
   }
 
   response.writeHead(200);
-  response.end(id +', Hello World! 6, ' + resStr.length);
+  response.end(id +', Hello World! 7, ' + resStr.length);
 };
 var www = http.createServer(handleRequest);
 www.listen(8080);
